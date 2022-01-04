341de121b0a1b05ffdeb1028126ff4bcb3e6f872
Nguyễn Mạnh Vũ <93993940+nmvu19@users.noreply.github.com>
2022-01-04 16:53:00 +0700

    Add reviews model
    reviews will be a subcollection of users documents
    users now have upvoteCount and totalVote field

--- model/userModel.js
+++ model/userModel.js
@@ -1,20 +1,22 @@
 import bcrypt from "bcrypt"
+import * as fs from "firebase-admin/firestore";
 
 const saltRound = 10;
+const debug = true;
 
 export default class userModel
 {
 	constructor(database)
 	{
-		this.db = database.collection("users");
+		this.usersRef = database.collection("users");
 	}
 
 	/**
 	 * 
-	 * @param {string} username 
-	 * @param {string} password 
-	 * @param {string} email 
-	 * @param {('bidder'|'seller'|'admin')} type 
+	 * @param {String} username 
+	 * @param {String} password 
+	 * @param {String} email 
+	 * @param {("bidder"|"seller"|"admin")} type 
 	 */
 	async newUser(username, password, email, type)
 	{
@@ -22,32 +24,131 @@ export default class userModel
 			throw new Error(`Invalid user type: ${type}`);
 
 		let hash = bcrypt.hashSync(password, saltRound);
-		let userDoc = this.db.doc(username);
-		await userDoc.set({
+		let doc = this.usersRef.doc(username);
+		await doc.set({
 			password: hash,
 			email: email,
-			type: type
+			type: type,
+			upvoteCount: 0,
+			totalVote: 0
 		});
 
-		console.log(`User ${username} written to database`);
+		if (debug)
+			console.log(`User ${username} written to database`);
 	}
 
 	/**
 	 * 
-	 * @param {string} username 
-	 * @param {string} password 
+	 * @param {String} username 
+	 * @param {String} password 
 	 * @returns compared result
 	 */
 	async checkPassword(username, password)
 	{
-		let userDoc = await this.db.doc(username).get();
+		let userDoc = await this.usersRef.doc(username).get();
 		return bcrypt.compareSync(password, userDoc.get("password"));
 	}
-	
+
+	async getUser(username)
+	{
+		let userDoc = await this.usersRef.doc(username).get();
+		let data = userDoc.data();
+		delete data.password;
+		delete data.email;
+		return data;
+	}
+
+	/**
+	 * Do not call this function
+	 * @param {string} username 
+	 * @param {firestore.QuerySnapshot} oldDoc 
+	 * @param {boolean} upvote 
+	 */
+	async updateVoteCount(username, oldDoc, upvote)
+	{
+		let oldData = oldDoc.data();
+		let change = {upvote: 0, total: 0};
+		console.log("old data");
+		console.log(oldData);
+		if (oldData) {
+			if (oldData.upvote != upvote) {
+				if (upvote)
+					change.upvote = 1
+				else 
+					change.upvote = -1;
+			}
+		}
+		else {
+			change.total = 1;
+			if (upvote)
+				change.upvote = 1;
+		}
+
+		console.log(change);
+		await this.usersRef.doc(username).update({
+			upvoteCount: fs.FieldValue.increment(change.upvote),
+			totalVote: fs.FieldValue.increment(change.total)
+		});
+
+		console.log(`Update vote count for user ${username}`)
+	}
+
+	/**
+	 * Add a user-to-user review. This won't check if the rater or the rated exists or not
+	 * @param {string} rater username of the rating user
+	 * @param {string} rated username of the rated user
+	 * @param {boolean} upvote true for thumbs up (+1), false for thumbs down (-1)
+	 * @param {string} review
+	 */
+	async addReview(rater, rated, upvote, review)
+	{
+		let reviewRef = this.usersRef.doc(rated).collection("reviews").doc(rater);
+
+		this.updateVoteCount(rated, await reviewRef.get(), upvote);
+
+		await reviewRef.set({
+			upvote: upvote,
+			review: review,
+			time: fs.Timestamp.fromDate(new Date())
+		}, {merge: true});
+		
+		if (debug)
+			console.log(`Updated the review of ${rated} by ${rater}`);
+	}
+
+	/**
+	 * @param {*} username
+	 * @param {*} start default: 0
+	 * @param {*} count default: 5
+	 * @param {("time"|"upvote")} order default: "time"
+	 * @returns reviews {
+	 *		username: {
+	 *	 		upvote: boolean, 
+	 *			review: string
+	 *		}
+	 *	}
+	 */
+	async getReviews(username, start = 0, count = 5, order = "time")
+	{
+		let docs = await this.usersRef
+									.doc(username)
+									.collection("reviews")
+									.orderBy(order)
+									.startAfter(start)
+									.limit(count)
+									.get();
+		
+		return docs.docs.map((doc) => {
+			let data = doc.data();
+			data.time = data.time.toDate();
+			return data;
+		});
+	}
+
 	/*
 	async checkPasswordByEmail(email, password)
 	{
-		let userDoc = await this.db.where("email", "==", email).get();
+		let userDoc = await this.usersRef.where("email", "==", email).get();
 		return bcrypt.compareSync(password, userDoc.password);
 	}
 	*/

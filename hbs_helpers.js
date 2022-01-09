3014d16396a1279f4fee5ad0a632405ee13d540d
Nguyễn Mạnh Vũ <93993940+nmvu19@users.noreply.github.com>
2022-01-10 01:53:32 +0700

    Add navbar and helpers

--- hbs_helpers.js
+++ hbs_helpers.js
@@ -1,8 +1,15 @@
 export default {
-	if_equal: function(a, b, option) { 
+	if_equal(a, b, option) { 
 		if (a == b)
 			return option.fn(this);
 		else    
 			return option.inverse(this);
+	},
+
+	section(name, options) {
+		if (!this._sections) {
+			this._sections = {};
+		}
+		this._sections[name] = options.fn(this);
 	}
 }

439b8697fa230f68f84998a900158f5268245171
Nguyễn Mạnh Vũ <93993940+nmvu19@users.noreply.github.com>
2022-01-09 18:05:29 +0700

    Add full text search (search by query)
    Also split main and extra image upload

--- model/itemModel.js
+++ model/itemModel.js
@@ -1,4 +1,5 @@
 import * as firestore from "firebase-admin/firestore";
+import lunr from "lunr-mutable-indexes";
 
 const fileDuration = 10000000000;
 const debug = true;
@@ -20,26 +21,71 @@ async function getUrl(file)
 	return res[0]
 }
 
-export default class userModel
+/**
+ * @param {QuerySnapshot} doc
+ * @returns {item} item data
+ */
+function parseItemDoc(doc)
+{
+	let data = doc.data();
+	if (!data)
+		return null;
+	
+	data.id = doc.id;
+	for (let prop in data)
+		if (data[prop] instanceof firestore.Timestamp)
+			data[prop] = data[prop].toDate();
+	
+	return data;
+}
+
+export default class itemModel
 {
 	constructor(database, bucket)
 	{
 		this.itemsRef = database.collection("auctionItems");
 		this.categoryRef = database.collection("categories");
-		this.bucket = bucket;	
+		this.bucket = bucket;
+
+		this.index = lunr(function() {
+			this.field("name");
+			this.field("description");
+			this.field("seller");
+			this.field("category");
+		})
+
+		this.createIndex();
 	}
 
-	/**
-	 * Do not call
-	 * @param {QuerySnapshot} doc
-	 * @returns {item} item data
-	 */
-	parseItemData(doc)
+	async createIndex()
 	{
-		let data = doc.data();
-		data.id = doc.id;
-		data.postedTime = data.postedTime.toDate();
-		return data;
+		let items = await this.getAllItems();
+		
+		for (let item of items) 
+			this.updateIndex(item)
+		
+		console.log("Full-text search index has been built");
+	}
+
+	updateIndex(item)
+	{
+		this.index.add({
+			id: item.id,
+			name: item.name,
+			description: item.description,
+			seller: item.seller,
+			category: item.category
+		})
+	}
+
+	async getAllItems()
+	{
+		let snapshot = await this.itemsRef.get();
+		let res = [];
+		snapshot.forEach((doc) => {
+			res.push(parseItemDoc(doc));
+		});
+		return res;
 	}
 
 	/**
@@ -50,7 +96,7 @@ export default class userModel
 	async getItem(id)
 	{
 		let doc = await this.itemsRef.doc(id).get();
-		return this.parseItemData(doc);
+		return parseItemDoc(doc);
 	}
 
 	/**
@@ -64,13 +110,24 @@ export default class userModel
 	async getItemsByCategory(category, start = 0, count = 5, order = "postedTime")
 	{
 		let res = await this.itemsRef
+								.where("listing", "==", true)
 								.where("category", "==", category)
 								.orderBy(order)
 								.startAfter(start)
 								.limit(count)
 								.get();
 
-		return res.docs.map(doc => this.parseItemData(doc));
+		return res.docs.map(doc => parseItemDoc(doc));
+	}
+
+	async getItemByQuery(query)
+	{
+		//console.log(this.index);
+		let searchRes = this.index.search(query);
+		let res = [];
+		for (var val of searchRes) 
+			res.push(val.ref);
+		return res;
 	}
 
 	/**
@@ -144,36 +201,35 @@ export default class userModel
 	/**
 	 * Add item image to storage
 	 * @param {string} id item id
-	 * @param data
-	 * ```
-	 * {
-	 * 	mainImage: binary
-	 * 	images: [binary]
-	 * }
-	 * ```
+	 * @param {binary} data
 	 */
-	async addImages(id, data)
+	async addMainImage(id, data)
 	{
-		let mainImageSave = null;
-		if (data.mainImage)
-			mainImageSave = this.bucket.file(`items/${id}/main.jpg`).save(data.mainImage);
-		
-		let imageSaves = [];
-		if (data.images && data.images.length)
-			for (let i = 0; i < data.images.length; i++)
-				imageSaves.push(this.bucket.file(`items/${id}/${i}.jpg`).save(data.images[i]));
+		await this.bucket.file(`items/${id}/main.jpg`).save(data);
+		if (debug) 
+			console.log(`Uploaded main images for item ${id}`);
+	}
 
-		if (mainImageSave)
-			await mainImageSave;
+	/**
+	 * 
+	 * @param {string} id 
+	 * @param {[bianry]} images array of image (binary)
+	 */
+	async addExtraImages(id, images)
+	{
+		let imageSaves = [];
+		for (let i = 0; i < images.length; i++)
+			imageSaves.push(this.bucket.file(`items/${id}/${i}.jpg`).save(images[i]));
+			
 		for (let promise of imageSaves)
 			await promise;
 		
 		if (debug) 
-			console.log(`Uploaded ${imageSaves.length + 1} images for item ${id}`);
+			console.log(`Uploaded ${imageSaves.length} images for item ${id}`);
 	}
 
 	/**
-	 * Add item to database. Item must have: 
+	 * Add item to database. Item data structure: 
 	 * ```javascript
 	 * {
 	 * 	name: string,
@@ -185,10 +241,11 @@ export default class userModel
 	 * 	autoExtend: boolean,
 	 * 	seller: string,
 	 * 	startingPrice: number
-	 * 	mainImage: binary
-	 * 	images: [binaries]
+	 * 	mainImage?: binary
+	 * 	images?: [binaries]
 	 * }
 	 * ```
+	 * mainImage and images is optional
 	 * @param {itemData} item 
 	 * @returns {Promise<String>} item id
 	 */
@@ -201,6 +258,7 @@ export default class userModel
 			step: item.step,
 			description: item.description,
 			postedTime: item.postedTime,
+			expireTime: item.expireTime,
 			autoExtend: item.autoExtend,
 			seller: item.seller,
 			listing: true,
@@ -209,7 +267,13 @@ export default class userModel
 
 		let doc = await this.itemsRef.add(itemData);
 
-		this.addImages(doc.id, item);
+		this.updateIndex(itemData);
+
+		if (item.mainImage)
+			this.addMainImage(doc.id, item.mainImage);
+
+		if (item.images && item.images.length)
+			this.addExtraImages(id, item.images);
 
 		if (debug)
 			console.log(`Added item ${itemData.name} to database with id ${doc.id}`);

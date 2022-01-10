a24742a34195e4d7a92d4b426651e56e70a192e6
Nguyễn Năng Tiến <tien.nnang@gmail.com>
2022-01-10 09:31:29 +0700

    mail send

---
+++ mail/mail.js
@@ -0,0 +1,42 @@
+import nodemailer from "nodemailer";
+
+const email ='thoitienteam@gmail.com'
+
+export default class mailing{
+
+
+    constructor(){
+        this.transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 1000,
+            secure: false,
+            service: 'gmail',
+            auth: {
+              user: email,
+              pass: 'NNT22102001'
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+          });
+    }
+
+    sendMail(to, subject, text){
+
+        var mailOptions = {
+            from: email,
+            to: to,
+            subject: subject,
+            text:text
+        }
+
+        this.transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    }
+
+}

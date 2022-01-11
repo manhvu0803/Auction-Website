722be23b6ef871da6ff40fb8a913258fe9394c49
Nguyễn Năng Tiến <tien.nnang@gmail.com>
2022-01-11 20:57:47 +0700

    cannot bid yet

--- routes/item.route.js
+++ routes/item.route.js
@@ -9,11 +9,18 @@ router.get('/:id', async (req, res) => {
         const itemData = await item.getItem(proID);
         const mainImage = await item.getMainImageUrl(proID);
         const images = await item.getExtraImageUrls(proID);
+        const pastBids = await item.getBid(proID,3);
+        let highestBidder=-1;
+        if(pastBids.length>0){
+            highestBidder = item[0];
+        }
         res.render('vwProduct/detail.hbs', {
             itemData,
             mainImage,
             images,
-            pluralImages: images.length >= 1
+            pluralImages: images.length >= 1,
+            pastBids,
+            highestBidder,
     })}
     catch(err){
         console.log(err);
@@ -22,6 +29,7 @@ router.get('/:id', async (req, res) => {
 })
 
 router.post('/:id', async (req, res) => {
+    const proID = req.params.id;
     const itemData = await item.getItem(proID);
     res.redirect('/item/'+req.params.id)
 })

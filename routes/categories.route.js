ad77bae17faf40b3979801e8323eca4d98ac5f5c
Nguyễn Năng Tiến <tien.nnang@gmail.com>
2022-01-13 19:29:55 +0700

    add new Cat

--- routes/categories.route.js
+++ routes/categories.route.js
@@ -1,5 +1,8 @@
 import express from "express";
 import {user,item} from "../model/model.js";
+import NodeCache from "node-cache";
+
+const myCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 const router = express.Router();
 
@@ -24,13 +27,70 @@ router.get("/", async (req, res) => {
     res.render("categories", { categories: data, items: votes, home:true})
 })
 
+
+router.get('/delete/:category', async (req, res) => {
+    console.log(3+req.body)
+});
+router.get('/delete/:category/:subcategory', async (req, res) => {
+    console.log(4+req.body)
+});
+
+router.post('/add', async (req, res) => {
+    if(req.session.auth){
+        if(req.session.authUser.isAdmin)
+        {
+            await item.addCategory(req.body.category)
+            res.redirect('/admin/managecategory')
+        }
+        else
+            res.render('vwError/404')
+    }
+    else{
+        res.render('vwError/404')
+    }
+});
+router.post('/add/:category/', async (req, res) => {
+    if(req.session.auth){
+        if(req.session.authUser.isAdmin)
+        {
+            await item.addCategory(req.params.category,req.body.subcategory);
+            res.redirect('/admin/managecategory')
+        }
+        else
+            res.render('vwError/404')
+    }
+    else{
+        res.render('vwError/404')
+    }
+});
+
 router.get("/:category/:subcategory/:page", async (req, res) => {
     let current = parseInt(req.params.page);
-    let itemDatas=await item.getItemsByCategory(req.params.category,req.params.subcategory);
-    const end = itemDatas.length<5;
-    for(let i=0;i<itemDatas.length;i++){
-        // itemDatas[i].mainImage= await item.getMainImageUrl(itemDatas[i].id);
+    if(current<1)
+    {
+        res.redirect(":/category/:subcategory/1");
+        return
     }
+    let itemDatas
+    let start=false
+    if(current==1)
+    {
+        itemDatas=await item.getItemsByCategory(req.params.category,req.params.subcategory);
+        start=true
+    }else{
+        itemDatas=await item.getItemsByCategory(req.params.category,req.params.subcategory,myCache.get('tail'));
+        myCache.set('prev'+current,itemDatas[0],10000)
+    }
+    const end = itemDatas.length<5;
+    if(!end){
+        myCache.set('tail',itemDatas[4],10000)
+        itemDatas.pop()
+    }
+    
+    for(let i=0;i<itemDatas.length;i++){
+        itemDatas[i].mainImage= await item.getMainImageUrl(itemDatas[i].id);
+    }
+
     const cats = await item.getAllCategories();
     const names = cats.categories;
 
@@ -48,7 +108,7 @@ router.get("/:category/:subcategory/:page", async (req, res) => {
             current: current,
             next: current + 1,
             prev: Math.max(1, current - 1),
-            start: current<2,
+            start: start,
             end: end
         },
         categories: data,
